374cbb78c6acaf6f12b35e3bc044a6cc23ed157a
shivkant dubey <shivkd1908@gmail.com>
2023-02-23 01:57:07 +0530

    added the dresses

--- responsible-act-7116/src/pages/_app.js
+++ responsible-act-7116/src/pages/_app.js
@@ -1,16 +1,13 @@
-import Navbar from '@/components/navbar'
+// import Navbar from '@/components/navbar'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 
-import Navbar from '@/components/navbar'
 import LandingPage from './Home/LandingPage'
-import ArticleList from './Article'
-
+// <Navbar/>
 
 export default function App({ Component, pageProps }) {
   return(
   <ChakraProvider>
-    <Navbar/>
     <LandingPage/>
     <Component {...pageProps} />
   </ChakraProvider>
