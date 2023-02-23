3b0ff04f48e44dc569ca72a2d5d9303cea42e7c9
ParbhatKataria1 <varshagagangandhi@gmail.com>
2023-02-23 23:21:37 +0530

    parbhat-day-3

--- responsible-act-7116/src/pages/_app.js
+++ responsible-act-7116/src/pages/_app.js
@@ -1,17 +1,14 @@
 import Navbar from '@/components/navbar'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
-
-import Navbar from '@/components/navbar'
 import LandingPage from './Home/LandingPage'
-import ArticleList from './Article'
 
 
 export default function App({ Component, pageProps }) {
   return(
   <ChakraProvider>
     <Navbar/>
-    <LandingPage/>
+    {/* <LandingPage/> */}
     <Component {...pageProps} />
   </ChakraProvider>
   )
