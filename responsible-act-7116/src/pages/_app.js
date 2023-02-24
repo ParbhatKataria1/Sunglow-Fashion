c8e8bdde2f89a4c681b8af12f7e9510d0caee104
harshal-kitukale <harshalkitukale005@gmail.com>
2023-02-23 23:58:42 -0800

    added to branch

--- responsible-act-7116/src/pages/_app.js
+++ responsible-act-7116/src/pages/_app.js
@@ -4,10 +4,6 @@ import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import LandingPage from '../components/Anthropologie'
 // import Navbar from '@/components/navbar'
-import '@/styles/globals.css'
-import { ChakraProvider } from '@chakra-ui/react'
-
-import LandingPage from './Home/LandingPage'
 // <Navbar/>
 export default function App({ Component, pageProps }) {
   return(
